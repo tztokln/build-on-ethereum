20e8505f171e74f4ece1c711747aff985d54e298
elena <tztokln@gmail.com>
2022-02-12 07:35:20 -0500

    ready to start

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,19 @@ require("@nomiclabs/hardhat-waffle");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const LOCAL_URL = "http://127.0.0.1:8545/";
+// https://mainnet.infura.io/v3/ff8dc4ce071247a8a4635aa090cf4934
+
+const PRIVATE_KEY = '4a49a96d6fb949278cb6fc208e3fa789';
+
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    local: {
+      url: LOCAL_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
