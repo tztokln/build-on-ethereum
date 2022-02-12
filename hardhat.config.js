2e800d6890572784aaf90e482065d27f9f7da8c6
elena <tztokln@gmail.com>
2022-02-12 10:56:58 -0500

    done with issues

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,21 @@
-require("@nomiclabs/hardhat-waffle");
+const LOCAL_URL = "http://127.0.0.1:8545/"; 
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
-const LOCAL_URL = "http://127.0.0.1:8545/";
-// https://mainnet.infura.io/v3/ff8dc4ce071247a8a4635aa090cf4934
-
-const PRIVATE_KEY = '4a49a96d6fb949278cb6fc208e3fa789';
-
+const PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
 
 module.exports = {
+
   solidity: "0.7.3",
+
   networks: {
+
     local: {
+
       url: LOCAL_URL,
+
       accounts: [PRIVATE_KEY]
+
     }
+
   }
+
 };
